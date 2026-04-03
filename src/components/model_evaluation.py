82a388910f699e65fcd7c99895352559194afdce
Md.Samsul Arefin <samsularefin532@gmail.com>
2026-04-04 02:48:28 +0600

    Model Evaluation Pipeline DONE
    Continuous Integration - DONE

---
+++ src/components/model_evaluation.py
@@ -0,0 +1,88 @@
+from src.exception import CustomException
+from src.logger import get_logger
+from dotenv import load_dotenv
+from src.utils.main_utils import load_config
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from src.utils.main_utils import load_numpy_array_data
+from src.entity.config_entity import DataFeatureEngineeringConfig
+import mlflow.keras
+import tensorflow as tf
+import mlflow
+import dagshub
+import sys
+import os
+import json
+import pickle
+
+load_dotenv()
+logger = get_logger("ModelEvaluation")
+dagshub_token = os.getenv("CAPSTONE_TEST")
+ml_flow_tracking_url = "https://dagshub.com/Crosshairs532/Fake-news-Classifier-MLOPS.mlflow/"
+mlflow.set_tracking_uri(ml_flow_tracking_url)
+dagshub.init(repo_owner='Crosshairs532', repo_name='Fake-news-Classifier-MLOPS', mlflow=True)
+
+class ModelEvaluation: 
+    def __init__(self):
+        self.preprocessor_path = os.path.join("artifacts", "preprocessor", "preprocessor.pkl")
+        self.model_path = os.path.join("artifacts", "models", "model.pkl")
+        self.config_path = os.path.join("artifacts", "preprocessor", "feature_config.json")
+    def load_object(self, file_path: str):
+        try:
+            with open(file_path, 'rb') as f:
+                return pickle.load(f)
+        except Exception as e:
+            logger.error(f"Error loading object from {file_path}")
+            raise CustomException(e, sys)
+    def initiate_model_evaluation(self, test_arr):
+        logger.info("Model Evaluation Started.")
+
+        mlflow.set_experiment("model-evaluation")
+        with mlflow.start_run() as run:
+            try:
+                X_test, y_test = test_arr[:, :-1], test_arr[:, -1]
+                tokenizer = self.load_object(self.preprocessor_path)
+                config = load_config(self.config_path)
+                max_len = config['max_len']
+                model_data = self.load_object(self.model_path)
+                arch_dict = model_data["architecture"]
+
+
+
+                # Load the trained Model
+                model_data = self.load_object(self.model_path)
+                model = tf.keras.models.model_from_json(model_data["architecture"])
+                model.set_weights(model_data["weights"])
+                model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+                # predict
+                y_pred_probs = model.predict(X_test)
+                y_pred = (y_pred_probs > 0.5).astype("int32")
+
+                acc = accuracy_score(y_test, y_pred)
+                precision = precision_score(y_test, y_pred)
+                recall = recall_score(y_test, y_pred)
+                f1 = f1_score(y_test, y_pred)
+
+                mlflow.log_metric("accuracy", acc)
+                mlflow.log_metric("precision", precision)
+                mlflow.log_metric("recall", recall)
+                mlflow.log_metric("f1_score", f1)
+
+                logger.info(f"model Evaludation Completed\n Accuracy: {acc}\n Precision: {precision}\n Recall: {recall}\n F1 Score: {f1}")
+
+            except Exception as e:
+                raise CustomException(e, sys)
+
+if __name__ == "__main__":
+    """
+        1. data
+        2. model.pkl
+        3. preprocessor.pkl
+        4. feature_config.json
+        5. experiment_info.json
+    """
+    
+    model_evaluation = ModelEvaluation()
+    config = DataFeatureEngineeringConfig()
+    test_arr = load_numpy_array_data(config.test_arr_file_path)
+    model_evaluation.initiate_model_evaluation(test_arr)
--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,161 +1,3 @@
-# from src.logger import get_logger
-# from sklearn.model_selection import train_test_split
-# import tensorflow
-# from tensorflow import keras
-# from keras.layers import LSTM, Dense
-# from keras.layers import Embedding
-# from keras import Sequential, Input
-# from src.exception import CustomException
-# from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
-# import sys
-# from src.utils.main_utils import *
-# import os
-# import mlflow
-# import mlflow.keras
-# import dagshub
-
-# # Disable GPU for tensorflow to prevent LSTM hanging on Mac
-# tf_gpus = tensorflow.config.list_physical_devices('GPU')
-# if tf_gpus:
-#     try:
-#         tensorflow.config.set_visible_devices([], 'GPU')
-#         logger.info("Disabled tensorflow-metal GPU acceleration to avoid LSTM hanging bugs.")
-#     except RuntimeError as e:
-#         logger.error(e)
-# ml_flow_tracking_url = "https://dagshub.com/Crosshairs532/Fake-news-Classifier-MLOPS.mlflow/"
-
-# mlflow.set_tracking_uri(ml_flow_tracking_url)
-# dagshub.init(repo_owner='Crosshairs532', repo_name='Fake-news-Classifier-MLOPS', mlflow=True)
-
-# logger = get_logger('Model training')
-
-# class ModelTrainer: 
-#     def __init__(self, feature_engineering_artifact):
-#         self.feature_engineering_artifact = feature_engineering_artifact
-#         self.token_size = None
-#         self.max_len = None
-#         self.scores = {}
-
-#     def create_model(self):
-#         model = Sequential([
-#             Input(shape=(self.max_len,)),
-#             Embedding(self.token_size, 32, input_length=self.max_len),
-#             LSTM(32),
-#             Dense(1, activation='sigmoid')
-#         ])
-#         model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-#         return model
-
-#     def model_object(self, train_arr, test_arr):
-#         try:
-#             x_train, y_train = train_arr[:, :-1], train_arr[:, -1]
-#             x_test, y_test = test_arr[:, :-1], test_arr[:, -1]
-
-
-#             print("x_train.shape",x_train.shape)
-#             print("y_train.shape",y_train.shape)
-#             print("x_test.shape",x_test.shape)
-#             print("y_test.shape",y_test.shape)
-
-#             # logger.info(f"Train shape: {x_train.shape}, Test shape: {x_test.shape}")
-
-#             model = self.create_model()
-
-#             logger.info('Model training started')
-#             model.fit(x_train, y_train, epochs=5, batch_size=16, validation_data=(x_test, y_test))
-#             # model.fit(x_train, y_train, epochs=5, batch_size=16)
-            
-#             y_pred = (model.predict(x_test) > 0.5).astype('int8')
-            
-#             self.scores = {
-#                 "accuracy": accuracy_score(y_test, y_pred),
-#                 "f1": f1_score(y_test, y_pred),
-#                 "precision": precision_score(y_test, y_pred),
-#                 "recall": recall_score(y_test, y_pred)
-#             }
-
-#             return model
-
-#         except Exception as e: 
-#             raise CustomException(e, sys)
-    
-
-#     def save_model_info(run_id: str, model_path: str, file_path: str) -> None:
-
-#         try:
-#             model_info = {'run_id': run_id, 'model_path': model_path}
-#             with open(file_path, 'w') as file:
-#                 json.dump(model_info, file, indent=4)
-#             logger.debug('Model info saved to %s', file_path)
-#         except Exception as e:
-#             logger.error('Error occurred while saving the model info: %s', e)
-#             raise
-
-#     def initiate_model_trainer(self, train_arr, test_arr):
-        
-
-#         logger.info("Model Training Stasrted")
-#         logger.info('loading feature config..')
-
-#         mlflow.set_experiment('Fake-News-Classifier-MLOPS')
-
-#         with mlflow.start_run() as run: 
-
-#             feature_config = load_config('artifacts/preprocessor/feature_config.json')
-#             self.token_size = feature_config['vocab_size']
-#             self.max_len = feature_config['max_len']
-
-#             #log params
-#             mlflow.log_param('Vocab_size', self.token_size)
-#             mlflow.log_param('max_len', feature_config['max_len'])
-
-
-#             model = self.model_object(train_arr, test_arr)
-#             model_dir = os.path.join("artifacts", "models")
-#             os.makedirs(model_dir, exist_ok=True)
-#             save_path = os.path.join(model_dir, 'model.pkl')
-#             save_object(model, save_path)
-
-#             mlflow.log_artifact(save_path)
-#             mlflow.log_artifact("artifacts/preprocessor/preprocessor.pkl")
-#             mlflow.log_artifact("artifacts/preprocessor/feature_config.json")
-
-
-#             mlflow.log_metric("accuracy", self.scores["accuracy"])
-#             mlflow.log_metric("f1_score", self.scores["f1"])
-#             mlflow.log_metric("precision", self.scores["precision"])
-#             mlflow.log_metric("recall", self.scores["recall"])
-
-#             mlflow.keras.log_model(model, "FakeNewsClassifier")
-
-
-#             self.save_model_info(run.info.run_id, "artifacts/models", 'reports/experiment_info.json')
-
-#             logger.info('Model Saved')
-
-# if __name__ == "__main__":
-#     from src.entity.config_entity import DataFeatureEngineeringConfig
-#     from src.entity.artifact_entity import DataFeatureEngineerArtifact
-#     from src.utils.main_utils import load_numpy_array_data
-#     import os
-    
-#     config = DataFeatureEngineeringConfig()
-#     artifact = DataFeatureEngineerArtifact(
-#         preprocessor_file_path=config.preprocessor_file_path,
-#         feature_config_file_path=config.feature_config_file_path
-#     )
-    
-#     train_arr = load_numpy_array_data(config.train_arr_file_path)
-#     test_arr = load_numpy_array_data(config.test_arr_file_path)
-    
-#     trainer = ModelTrainer(artifact)
-#     trainer.initiate_model_trainer(train_arr, test_arr)
-        
-        
-
-
-        
-       
 from src.logger import get_logger
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
@@ -164,6 +6,7 @@ from keras.layers import LSTM, Dense, Embedding, Dropout
 from keras import Sequential, Input
 from src.exception import CustomException
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+
 import sys
 from src.utils.main_utils import *
 import os
@@ -211,19 +54,20 @@ class ModelTrainer:
         model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
         return model
  
-    def model_object(self, train_arr, test_arr):
+    def model_object(self, train_arr):
         try:
-            x_train, y_train = train_arr[:, :-1], train_arr[:, -1]
-            x_test, y_test = test_arr[:, :-1], test_arr[:, -1]
+            # x_train, y_train = train_arr[:, :-1], train_arr[:, -1]
+            # x_test, y_test = test_arr[:, :-1], test_arr[:, -1]
  
-            y_train = y_train.astype('int32')
-            y_test = y_test.astype('int32')
+            # y_train = y_train.astype('int32')
+            # y_test = y_test.astype('int32')
  
-            print("x_train.shape", x_train.shape)
-            print("y_train.shape", y_train.shape)
-            print("x_test.shape", x_test.shape)
-            print("y_test.shape", y_test.shape)
- 
+            # print("x_train.shape", x_train.shape)
+            # print("y_train.shape", y_train.shape)
+            # print("x_test.shape", x_test.shape)
+            # print("y_test.shape", y_test.shape)
+            x_train, x_test, y_train, y_test = train_test_split(train_arr[:, :-1], train_arr[:, -1], test_size=0.2, random_state=42)
+            
             model = self.create_model()
  
             logger.info('Model training started')
@@ -314,4 +158,4 @@ if __name__ == "__main__":
     test_arr = load_numpy_array_data(config.test_arr_file_path)
     
     trainer = ModelTrainer(artifact)
-    trainer.initiate_model_trainer(train_arr, test_arr)
+    trainer.initiate_model_trainer(train_arr)
